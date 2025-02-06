86afd4404ac9404b0fca79dccc3580a134bbd639
elish9571 <elish9571@gmail.com>
2025-02-06 04:41:31 +0200

    add again - first commit

---
+++ GitHubPortfolioService/GitHubIntegrationOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubPortfolioService
+{
+    public class GitHubIntegrationOptions
+    {
+        public int UserName { get; set; }
+        public int Token { get; set; }
+    }
+}
